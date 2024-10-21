b816ebf67987f372297c084241a096f30ee522d3
unknown <your@email.example>
2024-10-21 14:53:57 +0300

    Updated v2

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Edit post with id{$post->id}</h1>
+
+    <form action="/posts/{{$post->id}}/update" method="post">
+        @csrf
+        @method('PUT')
+        
+        <label for="title">Title: </label>
+        <input type="text" id="title" name="title" value ="{{$post ->title}}">
+        <br><br>
+        <label for="content">Content: </label>
+        <textarea name="content" id="content">{{$post ->content}}</textarea>
+        <input type="submit" value="Create">
+    </form>
+</body>
+</html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,12 +11,15 @@
 <body>
     <h1>All posts</h1>
     <a href="/posts/create">Create a post</a>
+
     <ul>
         @foreach ($allPosts as $post)
             <li>
                 Title: {{ $post->title }} <br>
                 Content: {{ $post->content }} <br>
                 <a href="/posts/{{ $post->id }}">Show</a>
+                <a href="/posts/{{ $post->id }}/edit">Edit</a>
+
             </li>
         @endforeach
     </ul>
